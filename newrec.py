cc7eca9f5cf772c8f127a60ef29913a8f4bb8df4
lbaitemple <lbai@temple.edu>
2016-12-11 19:51:37 -0500

    upload the change
    This one can auto detect the silence

--- newrec.py
+++ newrec.py
@@ -12,24 +12,20 @@ from gtts import gTTS
 
  
 def speak_text(language, phrase):
-#    googleSpeechURL = "http://translate.google.com/translate_tts?"+str("ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&tl=") + language +"&q=" + phrase
-#    print googleSpeechURL
-#    makeURL = str("wget -q -U Mozilla -O output.mp3 \"") + googleSpeechURL + "\""
-#    print makeURL
-#    p2=subprocess.call([makeURL], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#    subprocess.call(['mplayer output.mp3'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     tts = gTTS(text=phrase, lang=language)
     tts.save("output.mp3")
     subprocess.call(['mplayer output.mp3'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
 
-def transcribe():
+def transcribe(lang):
     key = 'AIzaSyAcalCzUvPmmJ7CZBFOEWx2Z1ZSn4Vs1gg'
-    stt_url = 'https://www.google.com/speech-api/v2/recognize?client=chromium&lang=en-us&key=' + key
+    stt_url = 'https://www.google.com/speech-api/v2/recognize?client=chromium&lang='+ lang + '&key=' + key
     filename = 'test.flac'
     print "listening .."
-    os.system(
-        'arecord -D plughw:1,0 -f cd -c 1 -t wav -d 0 -q -r 16000 -d 3 | flac - -s -f --best --sample-rate 16000 -o ' + filename)
+#    os.system(
+#        'arecord -D plughw:1,0 -f cd -c 1 -t wav -d 0 -q -r 16000 -d 3 | flac - -s -f --best --sample-rate 16000 -o ' + filename)
+    os.system('rec -r 16000 ' + filename + ' silence 1 0.1 5% 1 2.0 5%')
+
     print "interpreting .."
     # send the file to google speech api
     c = pycurl.Curl()
@@ -72,23 +68,12 @@ class Translator(object):
         self.headers = {'Authorization': 'Bearer ' + oauth_junk['access_token']}
  
     def translate(self, origin_language, destination_language, text):
-#        german_umlauts = {
-#            0xe4: u'ae',
-#            ord(u'ö'): u'oe',
-#            ord(u'ü'): u'ue',
-#            ord(u'ß'): None,
-#        }
  
         translation_args = {
             'text': text,
             'to': destination_language,
             'from': origin_language
         }
-#        translation_result = requests.get(Translator.translation_url + urllib.urlencode(translation_args),
-#                                          headers=self.headers)
-#        translation = translation_result.text[2:-1]
-        #gs = goslate.Goslate(writing=goslate.WRITING_NATIVE_AND_ROMAN)
-        #translation = gs.translate(text, destination_language)
 	client = azure_translate_api.MicrosoftTranslatorClient('TranslationPi',  # make sure to replace client_id with your client id
                                                        'l8Hhx/ejsWLIaTAcu/f7TV04epEqQM3WwmB5Da1iPp0=') # replace the client secret with$
 	translation =  client.TranslateText(text, origin_language, destination_language)
@@ -107,4 +92,4 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--destination_language', help='Destination Language', required=True)
     args = parser.parse_args()
     while True:
-        Translator().translate(args.origin_language, args.destination_language, transcribe())
+        Translator().translate(args.origin_language, args.destination_language, transcribe(args.origin_language))
